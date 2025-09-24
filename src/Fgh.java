fedaab4d993e48abe940220cbe1d81d2a122030f
manuel tirado <tiradoriosmanuel@gmail.com>
2025-09-24 09:38:25 +0200

    prueba

---
+++ src/Fgh.java
@@ -0,0 +1,10 @@
+public class Fgh {
+    public static void main(String[] args) {
+        int suma = 0;
+
+        for (int i = 1; i < 100; i = i +2) {
+            suma = suma + i;
+        }
+        System.out.println("suma = " + suma);
+    }
+}
