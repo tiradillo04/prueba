90f35233e64aa63f92545c7fe6b8c1d6ae6fcfca
manuel tirado <tiradoriosmanuel@gmail.com>
2025-09-24 09:37:51 +0200

    prueba

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,5 +1,12 @@
 public class Prueba {
     public static void main(String[] args) {
-        
+        int suma = 0;
+
+        for (int i = 1; i < 11; i++) {
+            suma += i;
+        }
+
+        System.out.println("suma de los diez primeros numeros es igual" + suma);
+
     }
 }
