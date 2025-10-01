70691a24fcdbdbe8e90f5ebb726ea9fbdc0df3db
manuel tirado <tiradoriosmanuel@gmail.com>
2025-10-01 08:55:14 +0200

    prueba

--- src/ejercicio8.java
+++ src/ejercicio8.java
@@ -2,7 +2,13 @@ import java.util.Scanner;
 
 public class ejercicio8 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in) ;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("introduce un numero entero que sea positivo");
+        int numero = sc.nextInt();
 
+        int suma = 0;
+        for (int i = 1; i < numero; i++) {
+            if ()
+        }
     }
 }
