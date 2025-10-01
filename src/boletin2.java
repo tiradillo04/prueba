150e6ecca75c298d4e31b734236b788fdee9a49b
manuel tirado <tiradoriosmanuel@gmail.com>
2025-10-01 09:45:39 +0200

    prueba

---
+++ src/boletin2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class boletin2 {
+    //realizar un programa que solicita 3 numeros cualquiera y los muestre de menor a mayor.
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("introduce el primer numero");
+        int 1 = sc.nextInt();
+
+
+
+
+        System.out.println("introduce el segundo numero");
+        int 2 = sc.nextInt();
+
+
+
+
+        System.out.println("introduce el ultimo numero");
+        int 3 = sc.nextInt();
+
+        if (1 > 2 ){
+            1 = 1 + 2;
+            2 = 1 - 2;
+            1 = 1 - 2;
+        }
+        if (2 > 3){
+            2 = 2 + 3;
+            3 = 2 - 3;
+            2 =
+        }
+    }
+}
---
+++ src/boletin2_ejercicio2.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class boletin2_ejercicio2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in) ;
+
+    }
+}
