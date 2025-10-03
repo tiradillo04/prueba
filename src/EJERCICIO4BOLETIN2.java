6ca4c5155ac86eda16d5d654474cdeb756704170
manuel tirado <tiradoriosmanuel@gmail.com>
2025-10-03 09:18:58 +0200

    prueba

---
+++ src/EJERCICIO4BOLETIN2.java
@@ -0,0 +1,16 @@
+public class EJERCICIO4BOLETIN2 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int generarAleatorio (int max) {
+        int numAleatorio = (int) (Math.random()* max + 1);
+
+
+        return numAleatorio;
+    }
+
+    public static int generarAleatorio (int min , int max) {
+        return (int) (Math.random() * (max - min + 1)) + min;
+    }
+}
