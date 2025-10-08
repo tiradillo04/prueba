3163362ab1582e9391fa5437c5a72644bbfdd260
manuel tirado <tiradoriosmanuel@gmail.com>
2025-10-08 09:12:39 +0200

    prueba

--- src/ejercicio7boletin2.java
+++ src/ejercicio7boletin2.java
@@ -3,7 +3,7 @@ public class ejercicio7boletin2 {
 for (int i = 0; i <=10 ; i++){
     System.out.println();
     System.out.println("tabla del " +i);
-    for (int i = 0; i <=10 ; i++){
+    for (int j = 0; j <=10 ; j++){
         System.out.println(i+"x"+i+"="+i*i);
     }
         }
