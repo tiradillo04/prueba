0f3f2834ca6b79b52ff761fe220a4980e34317e2
manuel tirado <tiradoriosmanuel@gmail.com>
2025-10-14 10:44:00 +0200

    prueba

---
+++ src/apredo/ejemplOperadores.java
@@ -0,0 +1,14 @@
+package apredo;
+
+public class ejemplOperadores {
+    static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println("suma" + (a + b));
+        System.out.println("resta" + (a - b));
+        System.out.println("Multiplicacion" + (a + b));
+        System.out.println("modulo" + (a % b));
+    }
+}
