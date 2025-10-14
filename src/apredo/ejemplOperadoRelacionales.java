a855461594e182dc4ab8720bb81518d8add46c7a
manuel tirado <tiradoriosmanuel@gmail.com>
2025-10-14 11:00:27 +0200

    prueba

---
+++ src/apredo/ejemplOperadoRelacionales.java
@@ -0,0 +1,15 @@
+package apredo;
+
+public class ejemplOperadoRelacionales {
+    static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a > b); // false
+        System.out.println(a < b); // true
+        System.out.println(a == b); // false
+        System.out.println(a != b); // true
+
+    }
+}
