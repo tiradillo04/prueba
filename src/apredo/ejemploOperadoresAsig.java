2659127358964ec64fc45fd2fdf3989c7b3c0def
manuel tirado <tiradoriosmanuel@gmail.com>
2025-10-14 10:55:20 +0200

    prueba

---
+++ src/apredo/ejemploOperadoresAsig.java
@@ -0,0 +1,14 @@
+package apredo;
+
+public class ejemploOperadoresAsig {
+    static void main(String[] args) {
+        int y = 26;
+
+        y += 5; // a "y" se le suma ( en este caso 5)
+        y -= 13; // a "y" se le resta (en este caso 13)
+        y *= 7; // "y" se multiplica (7 en este caso)
+        y /= 4; // "y" se divide entre ( este caso 4)
+
+        System.out.println("resultado final : " + y);
+    }
+}
