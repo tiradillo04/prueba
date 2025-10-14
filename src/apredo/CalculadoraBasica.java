f6264b792283422002836bdaa77a49407b2b8168
manuel tirado <tiradoriosmanuel@gmail.com>
2025-10-14 11:14:50 +0200

    prueba

---
+++ src/apredo/CalculadoraBasica.java
@@ -0,0 +1,14 @@
+package apredo;
+
+public class CalculadoraBasica {
+    static void main(String[] args) {
+        int num1 = 27;
+        int num2 = 15;
+
+        System.out.println("suma = " + (num1 + num2));
+        System.out.println("resta = " + (num1 - num2));
+        System.out.println("multiplicacion = " + (num1 * num2));
+        System.out.println("division = " + (num1 / num2) + " , y modulo es  = " + (num1 % num2));
+
+    }
+}
