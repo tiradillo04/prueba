384cd9d7c4afcfc32dd92051609163128ed0603b
manuel tirado <tiradoriosmanuel@gmail.com>
2025-10-16 12:05:39 +0200

    prueba

---
+++ src/calculadoraBasicados.java
@@ -0,0 +1,13 @@
+public class calculadoraBasicados {
+    static void main(String[] args) {
+        int num1 = 27;
+        int num2 = 15;
+
+        System.out.println("Suma: " + (num1 + num2));
+        System.out.println("Resta: " + (num1 - num2));
+        System.out.println("Multiplicación: " + (num1 * num2));
+        System.out.println("División: " + ((double) num1 / num2));
+        System.out.println("Módulo: " + (num1 % num2));
+
+    }
+}
