cd874351df371a81884d964441c8794b49836302
manuel tirado <tiradoriosmanuel@gmail.com>
2025-10-16 12:46:59 +0200

    prueba

--- src/apredo/comparaciones.java
+++ src/apredo/comparaciones.java
@@ -7,5 +7,6 @@ public class comparaciones {
          System.out.println("¿ el 27 es mayor que el 15? : " + (numero1 > numero2)); // ">" indica que numero1 es mayor que numero2 y cuando ejecute me dira si es verdadero o falso
          System.out.println("¿ el 27 es igual que el 15? : " + (numero1 == numero2)); // "==" indica que son iguales y cuando ejecute me dira si es cierto o no
          System.out.println("¿ el numero 27 es distinto que el 15? : " + (numero1 != numero2));// "!=" indica que son distintos cuando ejecute dira si es cierto o no
+         System.out.println("¿ el numero 27 es mayor que 15 y 15 es mayor que 0? : " + ((numero1> numero2) && (numero2>0))) ;// hace que las dos condiciones sean verdaderas si una de las dos es falsa sera el resultado sera falso.
     }
 }
