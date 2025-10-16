bad83ff6f6c42d88b21a4fcd229f5e8f63c05ebb
manuel tirado <tiradoriosmanuel@gmail.com>
2025-10-16 12:31:35 +0200

    prueba

--- src/apredo/calculadoraBasicados.java
+++ src/apredo/calculadoraBasicados.java
@@ -2,14 +2,15 @@ package apredo;
 
 public class calculadoraBasicados {
     static void main(String[] args) {
-        int num1 = 27;
+        int num1 = 27;// hace que el numero 27 tenga
         int num2 = 15;
 
-        System.out.println("Suma: " + (num1 + num2));
-        System.out.println("Resta: " + (num1 - num2));
-        System.out.println("Multiplicación: " + (num1 * num2));
+        System.out.println("Suma: " + (num1 + num2));//suma el num1 al num2
+        System.out.println("Resta: " + (num1 - num2));// resta num1 - num2
+        System.out.println("Multiplicación: " + (num1 * num2));// multiplica num1*num2
         System.out.println("División: " + ((double) num1 / num2));
-        System.out.println("Módulo: " + (num1 % num2));
+        //hace division de num1 entre num2 (se utiliza el double para que de decimal)
+        System.out.println("Módulo: " + (num1 % num2));//hace el modulo de la division entre num1 y num2
 
     }
 }
---
+++ src/apredo/comparaciones.java
@@ -0,0 +1,11 @@
+package apredo;
+
+public class comparaciones {
+     public static void main(String[] args) {
+        int numero1 = 27;
+        int numero2 = 15;
+         System.out.println("¿ el 27 es mayor que el 15? : " + (numero1 > numero2)); // ">" indica que numero1 es mayor que numero2 y cuando ejecute me dira si es verdadero o falso
+         System.out.println("¿ el 27 es igual que el 15? : " + (numero1 == numero2)); // "==" indica que son iguales y cuando ejecute me dira si es cierto o no
+         System.out.println("¿ el numero 27 es distinto que el 15? : " + (numero1 != numero2));// "!=" indica que son distintos cuando ejecute dira si es cierto o no
+    }
+}
