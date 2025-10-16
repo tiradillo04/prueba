81c6a2cd499aaa9a00ff7a59195825c775ff215f
manuel tirado <tiradoriosmanuel@gmail.com>
2025-10-16 13:49:08 +0200

    prueba

---
+++ src/apredo/bucles.java
@@ -0,0 +1,29 @@
+package apredo;
+
+public class bucles {
+    static void main(String[] args) {
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("numero " + i);
+
+// paso uno , la inicializacion , [ int i = 0; ] creamos la variable i y le indicamos que empiece en 0.
+// paso dos , CONDICION  , [ i <= 5; ] cuando la variable i llega al numero 5(en este caso) cuando pase de 5 se detiene
+// paso trs , ACTUALIZACION , [ i++] en este caso se su suma 1 a i cada vez
+
+
+
+
+
+        }
+        {
+            System.out.println();
+            System.out.println();
+        }
+
+        int m = 0;// en el " while " se crea la variable fuera del while
+        while (m <= 5) {
+            System.out.println(m);
+           m++ ;
+        }
+
+    }
+}
