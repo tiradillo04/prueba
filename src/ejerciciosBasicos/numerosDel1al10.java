8448f6c9acffd5b7f2acb6fd6b6a5115788e3f83
manuel tirado <tiradoriosmanuel@gmail.com>
2025-10-21 11:05:56 +0200

    prueba

---
+++ src/ejerciciosBasicos/numerosDel1al10.java
@@ -0,0 +1,16 @@
+package ejerciciosBasicos;
+
+public class numerosDel1al10 {
+    static void main() {
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
+//Se inicializa i = 1.
+//
+//Se ejecuta mientras i(1) <= 10.
+//
+//En cada ciclo, i(1) aumenta en 1(ejemplo i(1)+ 1 = **** ;(i++)
+// asi hasta llegar a la condicion ( i <= 10;) cuando la i se va
+// sumando 1 hasta que *i + 1 = 10 * y para de ejecutarse.
